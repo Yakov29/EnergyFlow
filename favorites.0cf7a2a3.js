a89b752e89e64570e3b77dca67330ef347924108
JakeDer29 <108018683+Yakov29@users.noreply.github.com>
2024-08-20 12:55:50 +0300

    Updates

---
+++ favorites.0cf7a2a3.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".backdrop"),c=document.querySelector(".burger"),o=document.querySelector(".mobile__close");console.log(o),c.addEventListener("click",()=>{console.log("click"),e.classList.remove("change__invisible")}),o.addEventListener("click",()=>{e.classList.add("change__invisible")});
+//# sourceMappingURL=favorites.0cf7a2a3.js.map
